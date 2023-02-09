977c7dfba9eadb52f19838d23d6dd2dec1f462b7
dittaratana <dittarata13@gmail.com>
2023-02-09 19:23:28 +0700

    menambah health check dan get all id

--- node_modules/$root/scenario/get.all-id.booking.js
+++ node_modules/$root/scenario/get.all-id.booking.js
@@ -7,7 +7,7 @@ import restfulApi from '$root/page/restful.api';
 // Describe = seperti Suite in Qase.io
 describe('GetAllIdBooking', function() {
     //It = Test Case
-    it('Should succesful authentication', async () => {
+    it('Should succesful get all id booking', async () => {
         const response = await restfulApi.getallid();          
         console.log(response.data)
         assert.equal(response.status, 200);     
